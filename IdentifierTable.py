7db5d2677285aa3b64e3685d870c69ba35806a8b
kaden llewellyn <kadenlew@comcast.net>
2020-07-02 19:44:10 -0400

    identifier stuff

--- IdentifierTable.py
+++ IdentifierTable.py
@@ -1,3 +1,21 @@
+id_table = []
+
+class Identifier(object):
+    value = ""
+    def setValue(self, val):
+        self.value = val
+    def getValue(self):
+        return self.value
+
+class IdentifierTable(object):
+    def lookup(self, obj):
+        if obj in id_table:
+            return True
+        else:
+            return False
+
+    def store(self, obj):
+        id_table.append(obj);
 
 class PascalParser(object):
     pass
