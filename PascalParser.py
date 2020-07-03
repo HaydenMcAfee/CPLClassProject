25e95c06b04116c32aa7fb3d40f6998bb62471eb
Hayden <haydenmac24@gmail.com>
2020-07-02 20:26:56 -0400

    All the checks for Beginning and end done. My body is ready.

--- PascalParser.py
+++ PascalParser.py
@@ -6,11 +6,22 @@ class PascalParser(object):
         self.scanner = scanner
 
     def doParser(self):
-
-        if self.programNameCheck(): # Will check if the program is named
-            if self.loadImports(): # Will load imports if there are any and also look for keyword begin or else parse will fail
+        token = self.scanner.nextToken()
+        if self.programNameCheck(token): # Will check if the program is named
+            token = self.scanner.nextToken()
+            if self.loadImports(token): # Will load imports if there are any and also look for keyword begin or else parse will fail
+                token = self.scanner.nextToken()
+                self.checkBeg(token)
                 while 1:
                     token = self.scanner.nextToken()
+                    if self.checkEnd(token):
+                        token = self.scanner.nextToken()
+                        if not self.endOfStatement(token):
+                            break
+                        else:
+                            self.checkBeg(token)
+
+
                     # Will continue to read until token is none which means the end of the file has been reached
                     if token[0] is None:
                         break
@@ -25,27 +36,24 @@ class PascalParser(object):
         if token[1] == ";":
             return True
         else:
-            print("Invalid end of statement, expected ;")
             return False
 
     # We want to check if the program keyword is used and the following token is an identifier but not to store it
-    def programNameCheck(self):
-        token = self.scanner.nextToken()
+    def programNameCheck(self, token):
         if token[1] == 'program':
             token = self.scanner.nextToken()
             if token[0] == 'identifier':
                 token = self.scanner.nextToken()
                 if self.endOfStatement(token): # Check for ;
                     return True
+                else:
+                    raise NameError("Invalid end of statement, expected ;")
             else:
-                print("Program name not valid")
-                return False
+                raise NameError('Program name not valid')
         else:
-            print("Program name not declared")
-            return False
+            raise NameError("Program name not declared")
 
-    def loadImports(self):
-        token = self.scanner.nextToken()
+    def loadImports(self, token):
         while token[1] != 'begin':
             if token[1] == 'uses': # Program not set to begin, so we check if there's an import keyword
                 token = self.scanner.nextToken()
@@ -53,16 +61,33 @@ class PascalParser(object):
                     token = self.scanner.nextToken()
                     if self.endOfStatement(token):  # Check for ;
                         return True
+                    else:
+                        raise NameError("Invalid end of statement, expected ;")
                 else:
-                    print("Invalid library name")
-                    return False
+                    raise NameError("Invalid library name")
             else: # Triggers a fail if the import statement is not valid
-                print("Expected an import keyword")
-                return False
+                raise NameError("Expected an import keyword")
         # No imports to load and program has begin keyword
         return True
 
     def handleIdentfier(self):
         pass
+
+    # Checks if there is an end keyword and if its the end of the program or block
+    # Returns True if end of program, False if end of block, Excepts on anything else
+    def checkEnd(self, token):
+        if token[1] == "end":
+            token = self.scanner.nextToken()
+            if token[1] == '.':
+                return True
+            else:
+                return False
+
+    # Since the end token is not an end. then check for the beginning of another program block
+    def checkBeg(self, token):
+        if token[1] == "begin":
+            return True
+        else:
+            raise NameError('Expected begin keyword')
 
 
